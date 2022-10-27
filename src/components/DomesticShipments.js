e7c956aa8cc0dba606434d6455822b62b93beb32
fufako <patryko.mikiciuk@gmail.com>
2022-10-27 18:05:08 +0200

    Update DomesticShipments create ShipmentTypes component

--- src/components/DomesticShipments.js
+++ src/components/DomesticShipments.js
@@ -1,5 +1,19 @@
 const DomesticShipments = () => {
-  return
+  return (
+    <div className="domestic-shipments">
+      <h2 className="domestic-shipments__title">
+        Szeroki wybór przesyłek krajowych
+      </h2>
+    </div>
+  )
 }
 
 export default DomesticShipments
+
+const ShipmentType = () => {
+  return (
+    <div className="domestic-shipments__shipment">
+      <h2></h2>
+    </div>
+  )
+}
---
+++ src/components/ShipmentTypes.js
@@ -0,0 +1,12 @@
+const STANDARD = "Przesyłki standardowe"
+const PALLET = "Wysyłka palet"
+const CITY = "Przesyłki miejskie"
+const UNUSUAL = "Przesyłki nietypowe"
+
+const shipments = [{ type: STANDARD }]
+
+const ShipmentTypes = () => {
+  return
+}
+
+export default ShipmentTypes
