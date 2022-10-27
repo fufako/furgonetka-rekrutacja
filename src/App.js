808d4296a8f1cc4d909bf80dd4c03a7bd74d046f
fufako <patryko.mikiciuk@gmail.com>
2022-10-27 14:08:26 +0200

    Create and update Menu component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
-function App() {
-  return
+import Menu from "./components/Menu"
+const App = () => {
+  return <Menu />
 }
 
 export default App
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import FurgonetkaLogo from "../images/logo.svg"
+const Menu = () => {
+  return (
+    <>
+      <div className="menu">
+        <img src={FurgonetkaLogo} alt="furgonetka-logo" />
+      </div>
+    </>
+  )
+}
+
+export default Menu
