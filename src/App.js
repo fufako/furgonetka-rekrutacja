9609799e0fdaf9b0782fff1dec6ac08043b802c4
fufako <patryko.mikiciuk@gmail.com>
2022-10-27 15:29:34 +0200

    Update Promotion component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css"
 import Menu from "./components/Menu"
 import Header from "./components/Header"
+import Promotion from "./components/Promotion"
 const App = () => {
   return (
     <>
-      <Menu /> <Header />
+      <Menu /> <Header /> <Promotion />
     </>
   )
 }
--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -1,5 +1,19 @@
 const Promotion = () => {
-  return
+  return (
+    <div className="promotion">
+      <div className="promotion__container">
+        <h2 className="promotion__title">
+          Wybierz tanie i szybkie przesyłki krajowe
+        </h2>
+        <p className="promotion__description">
+          Wyceniaj i wysyłaj tanie paczki krajowe bez podpisywania umowy i
+          określania minimum logistycznego. U nas płacisz tylko za nadane
+          przesyłki. Nie pobieramy abonamentu za prowadzenie konta czy
+          korzystanie z dostępnych narzędzi.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Promotion
