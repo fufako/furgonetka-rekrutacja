5e603b2a6a30b21e4e27779b303a79719ad69ece
fufako <patryko.mikiciuk@gmail.com>
2022-10-29 14:25:24 +0200

    Clean code

--- src/components/DomesticShipments.js
+++ src/components/DomesticShipments.js
@@ -1,13 +1,12 @@
 import ShipmentTypes from "./ShipmentTypes"
-const DomesticShipments = () => {
-  return (
-    <section className="domestic-shipments">
-      <h2 className="domestic-shipments__title">
-        Szeroki wybór <span>przesyłek krajowych</span>
-      </h2>
-      <ShipmentTypes />
-    </section>
-  )
-}
+
+const DomesticShipments = () => (
+  <section className="domestic-shipments">
+    <h2 className="domestic-shipments__title">
+      Szeroki wybór <span>przesyłek krajowych</span>
+    </h2>
+    <ShipmentTypes />
+  </section>
+)
 
 export default DomesticShipments
