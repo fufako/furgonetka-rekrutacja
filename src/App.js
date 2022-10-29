c1c28efbfd15d20da64420ea04568ada9328dea7
fufako <patryko.mikiciuk@gmail.com>
2022-10-29 14:23:25 +0200

    Change name of menu to navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import "./App.css"
-import Menu from "./components/Menu"
+import "./styles/dist/main.css"
+import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import Promotion from "./components/Promotion"
 import DomesticShipments from "./components/DomesticShipments"
 import ShippingTools from "./components/ShippingTools"
 import Delivery from "./components/Delivery"
 import Partners from "./components/Partners"
-const App = () => {
-  return (
-    <>
-      <Menu />
-      <Header />
-      <Promotion />
-      <DomesticShipments />
-      <ShippingTools />
-      <Delivery />
-      <Partners />
-    </>
-  )
-}
+
+const App = () => (
+  <>
+    {/* TODO: change to navbar */}
+    <Navbar />
+    <Header />
+    <Promotion />
+    <DomesticShipments />
+    <ShippingTools />
+    <Delivery />
+    <Partners />
+  </>
+)
 
 export default App
