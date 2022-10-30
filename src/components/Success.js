3bf3d1f8911cfa60cd5962225bdaf7c0f94c290c
fufako <patryko.mikiciuk@gmail.com>
2022-10-30 16:59:59 +0100

    Create and update Success component

---
+++ src/components/Success.js
@@ -0,0 +1,12 @@
+const Success = () => {
+  return (
+    <div className="contact__success">
+      <h3 className="contact__success-title">Dziękujemy,</h3>
+      <p className="contact__success-description">
+        Twoje dane zostały poprawnie przesłane.
+      </p>
+    </div>
+  )
+}
+
+export default Success
