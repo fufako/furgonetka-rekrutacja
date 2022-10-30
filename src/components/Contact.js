c1147f583144a414293313482b4b42db98b76edb
fufako <patryko.mikiciuk@gmail.com>
2022-10-30 17:00:22 +0100

    Add conditional rendering in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,26 @@
+import { useState } from "react"
 import Form from "./Form"
+import Success from "./Success"
 
 const Contact = () => {
+  const [isFormSend, setIsFormSend] = useState(false)
+
   return (
     <section className="contact">
-      <h2 className="contact__title">
-        Zapytaj o <span>ofertę indywidualną</span>{" "}
-      </h2>
-      <p className="contact__description">
-        Nadajesz dużo przesyłek kurierskich krajowych? Napisz do nas,
-        a oddzwonimy, żeby porozmawiać o indywidualnej ofercie.
-      </p>
-      <Form />
+      {isFormSend ? (
+        <Success />
+      ) : (
+        <>
+          <h2 className="contact__title">
+            Zapytaj o <span>ofertę indywidualną</span>
+          </h2>
+          <p className="contact__description">
+            Nadajesz dużo przesyłek kurierskich krajowych? Napisz do nas, a
+            oddzwonimy, żeby porozmawiać o indywidualnej ofercie.
+          </p>
+          <Form setIsFormSend={setIsFormSend} />
+        </>
+      )}
     </section>
   )
 }
